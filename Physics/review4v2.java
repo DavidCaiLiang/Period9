c79b438677a6cb33fb18ea0b1f838be1526a824e
David Cai Liang <dcailiang40@stuy.edu>
2022-11-24 23:04:40 -0500

    I hate problem 7.

--- Physics/review4v2.java
+++ Physics/review4v2.java
@@ -101,11 +101,11 @@ public class review4v2{
       double counterPartial = 0;
       while ((counterFull + precision) < initialCompression + distance - (2 * springLength)) { //I did it this way becuase it is easy. I know I could probably do a continuous counter where we don't reset the counter after each phase of the process.
         keFull.add(0.5* springConstant * counterPartial * counterPartial - (coefficientOfFriction * mass * gravity * counterFull));
+        //System.out.println(0.5* springConstant * counterPartial * counterPartial - (coefficientOfFriction * mass * gravity * counterFull));
+        //System.out.println(0.5* springConstant * counterPartial * counterPartial);
+        //System.out.println(coefficientOfFriction * mass * gravity * counterFull);
         counterFull += precision;
-        if (counterPartial + precision >= springLength) {
-          counterPartial = springLength;
-        }
-        else {
+        if ((counterPartial + precision) >= compression) {
           counterPartial += precision;
         }
       }
@@ -113,23 +113,53 @@ public class review4v2{
     while (keAtSpringCollision > 0) {
       compression = (((-1 * coefficientOfFriction * mass * gravity) + Math.sqrt((coefficientOfFriction * coefficientOfFriction * mass * mass * gravity * gravity) + (2 * springConstant * keAtSpringCollision)))/ springConstant);
       compressions.add(compression);
-      //Stopped here.
+      double counter = 0;
+      while ((counter + precision) < compression) {
+        keFull.add(keAtSpringCollision - (0.5 * springConstant * counter * counter) - (coefficientOfFriction * mass * gravity * counter));
+        counter += precision;
+      }
+      //Stopped here. :< pain hell
       keAtSpringCollision = 0.5* springConstant * compression * compression - (coefficientOfFriction * mass * gravity * (compression + distance - (2 * springLength)));
       keAtSpringCollisions.add(keAtSpringCollision);
+      //fail rip -fix by getting rid the last set;
+      double counterFull = 0;
+      double counterPartial = 0;
+      while ((counterFull + precision) < compression + distance - (2 * springLength)) { //I did it this way becuase it is easy. I know I could probably do a continuous counter where we don't reset the counter after each phase of the process.
+        keFull.add(0.5* springConstant * counterPartial * counterPartial - (coefficientOfFriction * mass * gravity * counterFull));
+        counterFull += precision;
+        if (counterPartial + precision < compression) {
+          counterPartial += precision;
+        }
+      }
     }
-
-    System.out.println("Compression Array: "+compressions);
-    System.out.println("\nKinetic Energy at contact with Spring Array: "+keAtSpringCollisions);
-    System.out.println("The final value of the Kinetic Energy Array is supposed to negative!");
-
-    System.out.println("\nIs it going left to right: "+ (keAtSpringCollisions.size() % 2 == 1) + "\n");
+    //
+    //System.out.println("Compression Array: "+compressions);
+    //System.out.println("\nKinetic Energy at contact with Spring Array: "+keAtSpringCollisions);
+    //System.out.println("The final value of the Kinetic Energy Array is supposed to negative!");
+    //System.out.println("\nIs it going left to right: "+ (keAtSpringCollisions.size() % 2 == 1) + "\n");
     double deltaD = ((0.5 * springConstant * compressions.get(compressions.size()-1) * compressions.get(compressions.size()-1))-(coefficientOfFriction * mass * gravity * compressions.get(compressions.size()-1))/(springConstant * mass * gravity));
+    double counterFull = 0;
+    double counterPartial = 0;
+    while ((counterFull + precision) < compression + deltaD) { //I did it this way becuase it is easy. I know I could probably do a continuous counter where we don't reset the counter after each phase of the process.
+      keFull.add(0.5 * springConstant * counterPartial * counterPartial - (coefficientOfFriction * mass * gravity * counterFull));
+      //System.out.println(0.5 * springConstant * counterPartial * counterPartial - (coefficientOfFriction * mass * gravity * counterFull));
+      //System.out.println(0.5 * springConstant * counterPartial * counterPartial);
+      //System.out.println(coefficientOfFriction * mass * gravity * counterFull);
+      counterFull += precision;
+      if (counterPartial + precision < compression) {
+        counterPartial += precision;
+      }
+    }
+    for (int i = 0; i < keFull.size(); i++) {
+      velocity.add(Math.sqrt(keFull.get(i) * (1/mass) * 2));
+    }
      return velocity;
   }
 
   public static void main(String[] args) {
     System.out.println(finalPositionRelativeToBaseOfLeftSpring(0.05,120.0,1.0,0.25,0.3,0.2));
     System.out.println(totalDistanceTraveled(0.05,120.0,1.0,0.25,0.3,0.2));
+    System.out.println(velocity(0.05,120.0,1.0,0.25,0.3,0.2, 0.0005));
   }
 
 }
